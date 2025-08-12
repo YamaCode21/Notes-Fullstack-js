2c97d6763761329abf03aebdb0ce275858c51a5e
YamaCode <bradt.chancan2@gmail.com>
2025-08-12 06:46:26 -0500

    Actualizando login fronted

--- frontend/src/components/InputText.tsx
+++ frontend/src/components/InputText.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Icon } from "@iconify/react";
 
 interface InputTextProps {
@@ -6,6 +6,8 @@ interface InputTextProps {
   placeholder: string;
   onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
   className?: string;
+  value: string;
+  isPassword?: boolean;
 }
 
 const InputText: React.FC<InputTextProps> = ({
@@ -13,23 +15,50 @@ const InputText: React.FC<InputTextProps> = ({
   placeholder,
   onChange,
   className = "",
+  value,
+  isPassword,
 }) => {
+  const [showPass, setShowPass] = useState(false);
+
+  const togglePassword = () => {
+    setShowPass((prev) => !prev);
+  };
+
   return (
-    <div className="relative bg-gray-800 outfit-medium rounded-md !px-2 !py-7 !pb-2">
+    <div className="relative shadow-lg w-3/4 bg-gray-800 outfit-medium rounded-md !px-2 !py-7 !pb-2">
       <input
-        type={type}
+        type={isPassword ? (showPass ? "text" : "password") : type}
         onChange={onChange}
+        value={value}
         placeholder=" " // espacio para activar peer-placeholder-shown
         className={`peer w-full bg-gray-800 text-white text-md rounded focus:outline-none ${className}`}
       />
       <label
-        className="absolute left-2 text-slate-500 transition-all duration-200  pointer-events-none
-                   top-1/2 -translate-y-1/2
-                   peer-placeholder-shown:top-1/2 peer-placeholder-shown:text-lg 
-                   peer-focus:top-4 peer-focus:text-sm peer-focus:text-slate-400"
+        className={`absolute w-full left-2 text-slate-500 transition-all duration-200 pointer-events-none
+                    ${
+                      value
+                        ? "!top-1.5 text-sm text-slate-400" // ✨ Si hay valor, lo mantiene arriba
+                        : "top-1/2 -translate-y-1/2 peer-placeholder-shown:top-1/2 peer-placeholder-shown:text-lg"
+                    }
+                    peer-focus:top-4 peer-focus:text-sm peer-focus:text-slate-400
+                    `}
       >
-        {placeholder} <button><Icon icon="mdi:eye-off" width="24" height="24" style={{ color: "#fff" }}  /></button>
+        {placeholder}
       </label>
+      {isPassword === true && (
+        <button
+          type="button"
+          onClick={togglePassword}
+          className="absolute right-4 top-1/2 -translate-y-1/2 focus:outline-none"
+        >
+          <Icon
+            icon={showPass ? "mdi:eye" : "mdi:eye-off"}
+            width="24"
+            height="24"
+            style={{ color: "#fff" }}
+          />
+        </button>
+      )}
     </div>
   );
 };
--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -4,8 +4,17 @@ import "../styles/global.css";
 import InputText from "../components/InputText";
 
 const Login = () => {
+  
+  // Iniciar Sesión
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+
+  // Crear Cuenta
+  const [nombre, setNombre] = useState("");
+  const [newEmail, setNewEmail] = useState("");
+  const [newPassword, setNewPassword] = useState("");
+
+
   const [error, setError] = useState("");
 
   const [isFlipped, setIsFlipped] = useState(false);
@@ -27,43 +36,46 @@ const Login = () => {
   };
 
   return (
-    <div className="bg-gray-950 flex flex-col justify-center items-center h-screen gap-5">
+    <div className="bg-gray-900 flex flex-col justify-center items-center h-screen gap-5">
       <div className="flex gap-1 text-4xl outfit-black title-shadow">
         <p className="text-white">Tudu</p>
-        <p className="text-purple-500">List</p>
+        <p className="text-violet-500">List</p>
       </div>
-      <div className="bg-transparent w-[300px] h-[400px] perspective-[1000px]">
+      <div className="bg-transparent w-[400px] h-[400px] perspective-[1000px]">
         <div
           className={`relative w-full h-full transition-transform duration-700 rounded-2xl transform-style-preserve-3d ${
             isFlipped ? "rotate-y-180" : ""
           }`}
         >
           {/* Tarjeta de Inicio de Sesión (Front) */}
-          <div className="absolute w-full h-full backface-hidden bg-gray-700 rounded-lg shadow-lg p-8 flex flex-col justify-center items-center gap-5">
-            <h2 className="text-2xl outfit-bold mb-4 text-purple-500">
+          <div className="absolute w-full h-full backface-hidden bg-slate-700 rounded-lg shadow-lg p-8 flex flex-col justify-center items-center gap-5">
+            <h2 className="text-2xl outfit-bold mb-4 text-violet-500">
               Iniciar Sesión
             </h2>
             <form className="flex flex-col items-center gap-4 w-full">
               <InputText
                 type="email"
+                value={email}
                 placeholder="Correo"
-                onChange={() => {}}
+                onChange={(e) => setEmail(e.target.value)}
               />
               <InputText
                 type="password"
+                isPassword={true}
+                value={password}
                 placeholder="Contraseña"
-                onChange={() => {}}
+                onChange={(e) => setPassword(e.target.value)}
               />
               <button
                 type="submit"
-                className="bg-blue-500 w-1/2 !p-1 text-white rounded hover:bg-blue-600 transition"
+                className="bg-violet-600 w-1/3 !p-3 text-white outfit-bold hover:bg-violet-500 cursor-pointer rounded-2xl transition"
               >
                 Ingresar
               </button>
               <button
                 type="button"
                 onClick={toggleFlip}
-                className="text-blue-500 hover:underline"
+                className="text-violet-500 outfit-semibold hover:scale-105 transition-all cursor-pointer"
               >
                 Crear Cuenta
               </button>
@@ -71,34 +83,37 @@ const Login = () => {
           </div>
 
           {/* Tarjeta de Creación de Cuenta (Back) */}
-          <div className="absolute w-full h-full gap-5 backface-hidden bg-white rounded-lg shadow-lg p-8 flex flex-col justify-center items-center [transform:rotateY(180deg)]">
-            <h2 className="text-2xl font-bold mb-4">Crear Cuenta</h2>
+          <div className="absolute w-full h-full gap-5 backface-hidden bg-violet-800 rounded-lg shadow-lg p-8 flex flex-col justify-center items-center [transform:rotateY(180deg)]">
+            <h2 className="text-2xl font-bold text-white mb-4">Crear Cuenta</h2>
             <form className="flex flex-col items-center gap-4 w-full">
-              <input
+              <InputText
                 type="text"
+                value={nombre}
                 placeholder="Nombre de usuario"
-                className="p-2 border border-gray-300 rounded"
+                onChange={(e) => setEmail(e.target.value)}
               />
-              <input
+              <InputText
                 type="email"
+                value={newEmail}
                 placeholder="Correo electrónico"
-                className="p-2 border border-gray-300 rounded"
+                onChange={(e) => setEmail(e.target.value)}
               />
-              <input
+              <InputText
                 type="password"
-                placeholder="Contraseña"
-                className="p-2 border border-gray-300 rounded"
+                value={newPassword}
+                placeholder="Constraseña"
+                onChange={(e) => setEmail(e.target.value)}
               />
               <button
                 type="submit"
-                className="bg-blue-500 text-white p-2 rounded hover:bg-blue-600 transition"
+                className="bg-black w-1/3 outfit-bold text-white !p-3 rounded-xl hover:bg-gray-800 transition cursor-pointer"
               >
                 Registrarse
               </button>
               <button
                 type="button"
                 onClick={toggleFlip}
-                className="text-blue-500 hover:underline"
+                className="text-white outfit-semibold hover:scale-105 transition-all cursor-pointer"
               >
                 Volver a Iniciar Sesión
               </button>
